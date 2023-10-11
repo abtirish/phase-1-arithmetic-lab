837fa36dc8992e728fc1be73ac6863f75b8f5616
abtirish <abtirish@gmail.com>
2023-10-10 21:17:12 -0600

    finish phase 1 arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+function multiply(num1,num2){
+   return num1*num2;
+}
+
+var multiply = multiply(num1,num2);
+
+function random() {
+    return Math.floor(Math.random() * 100);
+}
+
+var random = random(1,100);
+
+console.log(random);
+
+
+let num4 = 12;
+let num3 = 3;
+function mod(num3,num4){
+    return num4/num3;
+}
+
+var mod = mod(num3,num4);
+
+console.log(mod);
+
+function max() {
+    return Math.max(1, 3, 5, 20);
+}
+
+var max = max(20);
+
+console.log(max);
